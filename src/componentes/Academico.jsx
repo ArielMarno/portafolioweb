d217e00775a47836f9e776561ca9f128e550b463
ArielMarno <arielmarno@outlook.com>
2024-03-20 11:09:31 -0300

    fix(about y academico: display y orientacion de cajas, tamaño de fuentes. Proyectos: cambio de mockup notebook en weather app. Footer: cambio de icono Outlook, tamaño de fuente de h2)

--- src/componentes/Academico.jsx
+++ src/componentes/Academico.jsx
@@ -9,21 +9,19 @@ import utn from '../assets/academico/utn.png';
 const Academico = () => {
   return (
     <div className='academico' id='academico'>
-        <article>
-            <h2>ACADEMICO</h2>
-            <p>Mis primeros pasos fueron participando del programa estatal, creado con la colaboración de la <span>Universidad Tecnologica Nacional</span>, "Argentina Programa" donde recibi una introducción a los fundamentos de la programación. Continue con mi capacitacion obteniendo el certificado de <span>Desarrollador Web Full Stack</span> en el <span>Instituto Municipal de Tecnología</span> y en la <span>Academia Numen</span>. 
-            <br />Hoy mi preferencia es por el desarrollo web frontend. Sigo trabajando y capacitandome de manera autodidacta en diseño UX/UI, nuevas tecnologias y frameworks para mejorar mi codigo y mis diseños.</p>
-        </article>
-        <aside>
-            <div>
-                <img src={arg} alt="logo argentina programa" />
-                <img src={imt} alt="logo instituto tecnologico" />
-            </div>
-            <div>
-                <img src={numen} alt="logo academia numen" />
-                <img src={utn} alt="logo utn" />
-            </div>
-        </aside>
+        <div>
+            <article>
+                <h2>ACADEMICO</h2>
+                <p>Mis primeros pasos fueron participando del programa estatal, creado con la colaboración de la <span>Universidad Tecnologica Nacional</span>, "Argentina Programa" donde recibi una introducción a los fundamentos de la programación. Continue con mi capacitacion obteniendo el certificado de <span>Desarrollador Web Full Stack</span> en el <span>Instituto Municipal de Tecnología</span> y en la <span>Academia Numen</span>. 
+                <br />Hoy mi preferencia es por el desarrollo web frontend. Sigo trabajando y capacitandome de manera autodidacta en diseño UX/UI, nuevas tecnologias y frameworks para mejorar mi codigo y mis diseños.</p>
+            </article>
+            <aside>
+                    <img src={arg} alt="logo argentina programa" />
+                    <img src={imt} alt="logo instituto tecnologico" />
+                    <img src={numen} alt="logo academia numen" />
+                    <img src={utn} alt="logo utn" />
+            </aside>
+        </div>
     </div>
   )
 }
--- src/componentes/Proyectos.jsx
+++ src/componentes/Proyectos.jsx
@@ -40,11 +40,11 @@ const Proyectos = () => {
 
             <section>
               <h3>Gericht Restaurant</h3>
-              <p>Diseño moderno y elegante para un restaurante de alta cocina. Cuenta con un navegador en la parte superior para recorrer la página de una manera más dinámica. Distintas secciones donde se puede encontrar: los especiales del día, carrusel de imágenes directamente desde su Instagram, un video mostrando el ambiente reconfortante del lugar y también una llamada a la acción donde el potencial cliente puede suscribirse para recibir novedades.</p>
+              <p>Diseño moderno y elegante para un restaurante de alta cocina. Cuenta con un navegador en la parte superior para recorrer la página de una manera más dinámica. Distintas secciones donde se puede encontrar: los especiales del día, carrusel de imágenes vinculado a su Instagram, un video mostrando el ambiente reconfortante del lugar y también una llamada a la acción donde el potencial cliente puede suscribirse para recibir novedades.</p>
               <div className='buttons'>
-                <a href="https://figmarestaurantapp.000webhostapp.com/" className='paginaweb'>PAGINA WEB</a>
-                <a href="https://github.com/ArielMarno/FineDiningRestaurantApp" className='github'>REPOSITORIO</a>
-                <a href="https://www.figma.com/file/yvClSI9AZBRX8UaaGEByF3/Modern-UI%2FUX%3A-Gericht?type=design&node-id=0-1&mode=design&t=HrlI32Pxpe5JdAD2-0" className='diseño'>DISEÑO FIGMA</a>
+                <a href="https://figmarestaurantapp.000webhostapp.com/" target="_blank" rel="noopener noreferrer" className='paginaweb'>PAGINA WEB</a>
+                <a href="https://github.com/ArielMarno/FineDiningRestaurantApp" target="_blank" rel="noopener noreferrer" className='github'>REPOSITORIO</a>
+                <a href="https://www.figma.com/file/yvClSI9AZBRX8UaaGEByF3/Modern-UI%2FUX%3A-Gericht?type=design&node-id=0-1&mode=design&t=HrlI32Pxpe5JdAD2-0" target="_blank" rel="noopener noreferrer" className='diseño'>DISEÑO FIGMA</a>
               </div>
             </section>
 
@@ -56,9 +56,9 @@ const Proyectos = () => {
               <h3>GPT-4 OpenAI</h3>
               <p>Blog sobre el desarrollo y la implementación de la inteligencia artificial. Diseño futurista y adaptable, con enlaces a las distintas secciones de la página, tarjetas con artículos sobre los temas más relevantes, imágenes atrapantes y títulos con colores degradados para resaltar el contenido.</p>
               <div className='buttons'>
-                <a href="https://gpt-4-ai.000webhostapp.com/" className='paginaweb'>PAGINA WEB</a>
-                <a href="https://github.com/ArielMarno/GPT-4" className='github'>REPOSITORIO</a>
-                <a href="https://www.figma.com/file/lz9lLpFHMxHm2odnwM3R0z/gpt3?type=design&node-id=0-1&mode=design&t=5EVD7mCtolYSN747-0" className='diseño'>DISEÑO FIGMA</a>
+                <a href="https://gpt-4-ai.000webhostapp.com/" target="_blank" rel="noopener noreferrer" className='paginaweb'>PAGINA WEB</a>
+                <a href="https://github.com/ArielMarno/GPT-4" target="_blank" rel="noopener noreferrer" className='github'>REPOSITORIO</a>
+                <a href="https://www.figma.com/file/lz9lLpFHMxHm2odnwM3R0z/gpt3?type=design&node-id=0-1&mode=design&t=5EVD7mCtolYSN747-0" target="_blank" rel="noopener noreferrer" className='diseño'>DISEÑO FIGMA</a>
               </div>
             </section>
 
@@ -82,9 +82,9 @@ const Proyectos = () => {
               <h3>Sunflower Agencia</h3>
               <p>Proyecto realizado para una agencia de marketing digital, diseño gráfico y publicidad en redes sociales. Se implementó un diseño minimalista y funcional para la oferta de servicios y el contacto directo con la agencia. Una sección dedicada a los trabajos realizados, con enlaces a páginas secundarias para ver los diseños e imágenes personalizadas de cada empresa o negocio. </p>
               <div className='buttons'>
-                <a href="https://sunfloweragencia.com/" className='paginaweb'>PAGINA WEB</a>
-                <a href="https://github.com/ArielMarno/sunflower" className='github'>REPOSITORIO</a>
-                <a href="https://www.figma.com/file/8Tp6eR73ilxO5CgIxko31z/Sunflower?type=design&node-id=0-1&mode=design&t=ZQOVwpgaOvCao126-0" className='diseño'>DISEÑO FIGMA</a>
+                <a href="https://sunfloweragencia.com/" target="_blank" rel="noopener noreferrer" className='paginaweb'>PAGINA WEB</a>
+                <a href="https://github.com/ArielMarno/sunflower" target="_blank" rel="noopener noreferrer" className='github'>REPOSITORIO</a>
+                <a href="https://www.figma.com/file/8Tp6eR73ilxO5CgIxko31z/Sunflower?type=design&node-id=0-1&mode=design&t=ZQOVwpgaOvCao126-0" target="_blank" rel="noopener noreferrer" className='diseño'>DISEÑO FIGMA</a>
               </div>
             </section>
 
@@ -96,9 +96,9 @@ const Proyectos = () => {
               <h3>Weather App</h3>
               <p>Aplicación web para conocer el clima en diferentes ciudades del mundo. Creada a partir de la API de OpenWeatherMap, muestra el clima actual y también un pronóstico extendido de hasta doce horas. El diseño cuenta con un panel lateral en el que se puede encontrar información adicional: el porcentaje de humedad, velocidad del viento, temperatura máxima y mínima, fecha y hora actual.</p>
               <div className='buttons'>
-                <a href="https://weatherwebside.000webhostapp.com/" className='paginaweb'>PAGINA WEB</a>
-                <a href="https://github.com/ArielMarno/weather-app" className='github'>REPOSITORIO</a>
-                <a href="https://www.behance.net/gallery/174927383/Weather-forecast-website" className='diseño'>DISEÑO BEHANCE</a>
+                <a href="https://weatherwebside.000webhostapp.com/" target="_blank" rel="noopener noreferrer" className='paginaweb'>PAGINA WEB</a>
+                <a href="https://github.com/ArielMarno/weather-app" target="_blank" rel="noopener noreferrer" className='github'>REPOSITORIO</a>
+                <a href="https://www.behance.net/gallery/174927383/Weather-forecast-website" target="_blank" rel="noopener noreferrer" className='diseño'>DISEÑO BEHANCE</a>
               </div>
             </section>
 
